86af98fe818ca32ca2a1bfb312fef34f2c8d4cb5
RedBoardDev <ott.thomas68@gmail.com>
2022-05-23 02:19:01 +0200

    (+) MEV info + MEV calcul for personal + reward luck rework

--- blocks/get_info_block.py
+++ blocks/get_info_block.py
@@ -1,7 +1,9 @@
+import get_luck
 from get_info import request_json
 
 #========================== INITIALIZE VARIABLE ==========================#
 crazyAPI_block = 'https://eth.crazypool.org/api/blocks'
+MPP_API_block = 'https://www.miningpoolsprofits.com/api/blocks?pool=CrazyPool&size=5&currentPage=0&coin=ETH'
 etherscanAPI_blockInfo = 'https://api.etherscan.io/api?module=block&action=getblockreward&blockno=BLOCK_ID&apikey=VFKV6IWHAFQ3X8P948TCQEFVASCT7YF9QU'
 
 #========================== FUNCTION ==========================#
@@ -12,20 +14,27 @@ def get_candidates():
 def check_if_uncle(blockMiner):
     return (blockMiner != "0x4f9bebe3adc3c7f647c0023c60f91ac9dffa52d5")
 
-def block_info(height, price_eth, luck):
+def block_info(height, price_eth):
     rsp_request = request_json(etherscanAPI_blockInfo.replace("BLOCK_ID", str(height)))['result']
+    rsp_request_MPP = request_json()
 
-    if (check_if_uncle(rsp_request['blockMiner'])):
-        print("blockMiner uncle:", rsp_request['blockMiner'])
-        height = height + 1
-        reward = 1.75
-        uncle = "True"
-    else:
-        reward = int(rsp_request['blockReward']) / 1000000000000000000
-        uncle = "False"
+    for block_info in rsp_request_MPP:
+        if (block_info['block_number'] == 14826057):
+            if (block_info['uncle'] == False):
+                MEV_profit:float = block_info['block_mev'] / 1000000000000000000
+                luck_block:float = block_info['current_luck_calculated']
+                reward = float(rsp_request['blockReward']) / 1000000000000000000
+            else:
+                MEV_profit:float = 0
+                luck_block:float = get_luck.luck_CP
+                height = height + 1
+                reward = 1.75
+                uncle = "True"
+            break
 
-    link = "https://etherscan.io/block/" + str(height)
-    message = "\nReward perso : " + "reward_for_me" + " | " + "reward_in_usd" + "$\n" + "Luck : " + (str)(luck) + '%\n'
-    message = message + "Uncle : " + uncle + "\nPrice ETH : " + str(price_eth) + "$\n" + link
-    message_l = ["Reward : " + str(round(reward, 9)) + " ETH", message, reward]
+    link = "https://etherscan.io/block/" + (str)(height)
+    message_all = "Reward : " + (str)(round(reward, 9)) + " ETH\nMEV: " + (str)(round(MEV_profit, 2)) + " ETH"
+    message_me = "\nReward perso : " + "reward_for_me" + " | " + "reward_in_usd" + "$\n" + "Luck : " + (str)(luck_block) + '%\n'
+    message_me = message_me + "Uncle : " + uncle + "\nPrice ETH : " + (str)(price_eth) + "$\n" + link
+    message_l = [message_all, message_me, reward + MEV_profit]
     return (message_l)
--- send_notification.py
+++ send_notification.py
@@ -1,11 +1,10 @@
-import get_luck
 from blocks.get_info_block import block_info
 from get_info import get_price_eth
 from get_info import request_json
 from funct_config import f_get_account
 from lib_discord import set_base_embed, set_embed_block, set_embed_payments
 
-def set_message(message:str, round_share, reward, price_eth):
+def set_reward_message(message:str, round_share, reward, price_eth):
     reward_for_me = (round_share * reward) / 100
     message = message.replace("reward_for_me", str(round(reward_for_me, 9)))
     reward_in_usd:float = reward_for_me * price_eth
@@ -20,12 +19,12 @@ def get_round_share(wallet:str):
 async def send_notif_block(height, bot):
     account_l = f_get_account()
     price_eth = get_price_eth()
-    message_l = block_info(height, price_eth, get_luck.luck_CP)
+    message_l = block_info(height, price_eth)
     for i in range(len(account_l)):
         i_account = account_l[i]
         if (i_account['settings']['blocks'] == True):
             round_share:float = get_round_share(i_account['wallet'])
-            message = set_message(message_l[1], round_share, message_l[2], price_eth)
+            message = set_reward_message(message_l[1], round_share, message_l[2], price_eth)
             channel = bot.get_channel(i_account['channel'])
             role_id = "<@&" + str(i_account['role_id']) + ">"
             embed = set_embed_block(set_base_embed("ETH | New block found !", "", 0x1ABC9C), message_l[0], message)
